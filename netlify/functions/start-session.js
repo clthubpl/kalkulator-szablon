95aed8bf539214409c110941e9cc2d764e5a3dc7
Baltyn Piotr <p.b@cltwitkowscy.pl>
2025-06-28 21:27:01 +0200

    Update start-session.js

--- netlify/functions/start-session.js
+++ netlify/functions/start-session.js
@@ -1,1 +1,9 @@
-console.log('Hello')
+// netlify/functions/start-session.js
+exports.handler = async function(event, context) {
+  const ip = event.headers["x-nf-client-connection-ip"] || "unknown";
+  // ... Twoja logika sesji ...
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Session started" }),
+  };
+}
