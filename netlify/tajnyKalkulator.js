414a9e02c8e698cb4950c0128e0a3a4b060bcfe1
CLT PL <cltplhub@gmail.com>
2025-07-10 23:19:45 +0200

    Update tajnyKalkulator.js

--- netlify/tajnyKalkulator.js
+++ netlify/tajnyKalkulator.js
@@ -1,14 +1,27 @@
-// netlify/functions/tajnyKalkulator.js
 exports.handler = async function(event, context) {
-  // Parsujemy dane JSON wysłane w body POST:
-  const { liczba1, liczba2 } = JSON.parse(event.body);
+  if (event.httpMethod === "OPTIONS") {
+    // Reagujemy na tzw. preflight (zapytanie wstępne wysyłane automatycznie przez przeglądarkę)
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",        // tu możesz podać konkretny origin
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
+      },
+      body: ""  // preflight nie potrzebuje ciała odpowiedzi
+    };
+  }
 
-  // Obliczamy sumę:
+  // Parsowanie danych jak wcześniej
+  const { liczba1, liczba2 } = JSON.parse(event.body);
   const suma = Number(liczba1) + Number(liczba2);
 
-  // Przygotowujemy odpowiedź z wynikiem:
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",        // pozwól na cross-origin
+      "Access-Control-Allow-Headers": "Content-Type"
+    },
     body: JSON.stringify({ result: suma })
   };
 };
