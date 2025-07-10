1c3d4f3de4780c5051cc2c571a8b5f7b3e656874
CLT PL <cltplhub@gmail.com>
2025-07-10 21:39:43 +0200

    Delete netlify/functions/start-session.js

--- netlify/functions/start-session.js
+++
@@ -1,9 +0,0 @@
-// netlify/functions/start-session.js
-exports.handler = async function(event, context) {
-  const ip = event.headers["x-nf-client-connection-ip"] || "unknown";
-  // ... Twoja logika sesji ...
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Session started" }),
-  };
-}
